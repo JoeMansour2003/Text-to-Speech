0bf581c3d95e4fb376706522bd15bba425b20c3f
Joe Mansour <joemansour2003@gmail.com>
2022-12-05 17:10:00 -0500

    Client.Html

--- index.js
+++ index.js
@@ -14,15 +14,51 @@ app.post("/extract-text", (req, res) => {
     }
 
     pdfParse(req.files.pdfFile).then(result => {
-        res.send(result.text);
         //here result.text= data
+        var audioFile = "test.mp3";
+        // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+        const speechConfig = sdk.SpeechConfig.fromSubscription(
+            "b974c7e81115411aa5076a392dad3380",
+            "eastus"
+        );
+        const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+    
+        // The language of the voice that speaks.
+        speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+        // Create the speech synthesizer.
+        var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+    
+        // var rl = readline.createInterface({
+        //   input: process.stdin,
+        //   output: process.stdout
+        // });
+    
+        // rl.question("Enter some text that you want to speak >\n> ", function (text) {
+        //   rl.close();
+        // Start the synthesizer and wait for a result.
+        synthesizer.speakTextAsync(
+            result.text,
+            function (result) {
+                if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+                    console.log("synthesis finished.");
+                } else {
+                    console.error(
+                        "Speech synthesis canceled, " +
+                            result.errorDetails +
+                            "\nDid you set the speech resource key and region values?"
+                    );
+                }
+                synthesizer.close();
+                synthesizer = null;
+            },
+            function (err) {
+                console.trace("err - " + err);
+                synthesizer.close();
+                synthesizer = null;
+            }
+        ).then(audio => {res.send(audio)});
+        console.log("Now synthesizing to: " + audioFile);
     });
 });
-
-app.get('/download', function(req, res){
-  const file = `${__dirname}/public/test.mp3`;
-  res.download(file); // Set disposition and send it.
-});
-
 
 app.listen(3000);
