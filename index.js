f1c418d1036c16e96b41de11e2dd7c302912a664
Joe Mansour <joemansour2003@gmail.com>
2022-12-06 14:30:00 -0500

    hello

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
-const express = require("express");
-const fileUpload = require("express-fileupload");
-const pdfParse = require("pdf-parse");
+// const server = require('./server');
 
-const app = express();
-// test
-app.use("/", express.static("public"));
-app.use(fileUpload());
+// const port = process.env.PORT || 3000;
 
-app.post("/extract-text", (req, res) => {
-    if (!req.files && !req.files.pdfFile) {
-        res.status(400);
-        res.end();
-    }
+// server.create()
+//     .then(app => {
+//         app.listen(port, () => {
+//             console.log(`Server has started on port ${port}!`);
+//         });
+//     }).catch(err => console.log(err));
 
-    pdfParse(req.files.pdfFile).then(result => {
-        //here result.text= data
-        var audioFile = "test.mp3";
-        // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-        const speechConfig = sdk.SpeechConfig.fromSubscription(
-            "b974c7e81115411aa5076a392dad3380",
-            "eastus"
-        );
-        const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+// const express = require("express");
+// const fileUpload = require("express-fileupload");
+// const pdfParse = require("pdf-parse");
+
+// const app = express();
+// // test
+// app.use("/", express.static("public"));
+// app.use(fileUpload());
+
+// app.post("/extract-text", (req, res) => {
+//     if (!req.files && !req.files.pdfFile) {
+//         res.status(400);
+//         res.end();
+//     }
+
+//     pdfParse(req.files.pdfFile).then(result => {
+//         //here result.text= data
+//         var audioFile = "test.mp3";
+//         // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+//         const speechConfig = sdk.SpeechConfig.fromSubscription(
+//             "b974c7e81115411aa5076a392dad3380",
+//             "eastus"
+//         );
+//         const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
     
-        // The language of the voice that speaks.
-        speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
-        // Create the speech synthesizer.
-        var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+//         // The language of the voice that speaks.
+//         speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+//         // Create the speech synthesizer.
+//         var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
     
-        // var rl = readline.createInterface({
-        //   input: process.stdin,
-        //   output: process.stdout
-        // });
+//         // var rl = readline.createInterface({
+//         //   input: process.stdin,
+//         //   output: process.stdout
+//         // });
     
-        // rl.question("Enter some text that you want to speak >\n> ", function (text) {
-        //   rl.close();
-        // Start the synthesizer and wait for a result.
-        synthesizer.speakTextAsync(
-            result.text,
-            function (result) {
-                if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-                    console.log("synthesis finished.");
-                } else {
-                    console.error(
-                        "Speech synthesis canceled, " +
-                            result.errorDetails +
-                            "\nDid you set the speech resource key and region values?"
-                    );
-                }
-                synthesizer.close();
-                synthesizer = null;
-            },
-            function (err) {
-                console.trace("err - " + err);
-                synthesizer.close();
-                synthesizer = null;
-            }
-        ).then(audio => {res.send(audio)});
-        console.log("Now synthesizing to: " + audioFile);
-    });
-});
+//         // rl.question("Enter some text that you want to speak >\n> ", function (text) {
+//         //   rl.close();
+//         // Start the synthesizer and wait for a result.
+//         synthesizer.speakTextAsync(
+//             result.text,
+//             function (result) {
+//                 if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+//                     console.log("synthesis finished.");
+//                 } else {
+//                     console.error(
+//                         "Speech synthesis canceled, " +
+//                             result.errorDetails +
+//                             "\nDid you set the speech resource key and region values?"
+//                     );
+//                 }
+//                 synthesizer.close();
+//                 synthesizer = null;
+//             },
+//             function (err) {
+//                 console.trace("err - " + err);
+//                 synthesizer.close();
+//                 synthesizer = null;
+//             }
+//         ).then(audio => {res.send(audio)});
+//         console.log("Now synthesizing to: " + audioFile);
+//     });
+// });
 
-app.listen(3000);
+// app.listen(3000);
